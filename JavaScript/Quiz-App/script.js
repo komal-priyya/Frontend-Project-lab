4d71928c7abfe9c42eeeb044c098cf3b8c0cffe3
Komal Priya <kryptonbenzene@gmail.com>
2026-06-11 22:48:47 +0530

    addes te next button and back button with functionalities

--- JavaScript/Quiz-App/script.js
+++ JavaScript/Quiz-App/script.js
@@ -1,51 +1,76 @@
 
-const quesBox= document.querySelector(".ques-box")
+const quesBox = document.querySelector(".ques-box")
 
-let Data=[
+let Data = [
     {
-      Question:"what is javascript",
-      options: ["language", "database","OS", "Browser"],
-      correctAnswer:"language"
+        Question: "what is javascript",
+        options: ["language", "database", "OS", "Browser"],
+        correctAnswer: "language"
     },
     {
-        Question:"Capital of India ",
-        options:["Delhi", "Mumbai","Chennai", "Chennai"],
-        correctAnswer:"Delhi"
+        Question: "Capital of India ",
+        options: ["Delhi", "Mumbai", "Chennai", "Chennai"],
+        correctAnswer: "Delhi"
     },
     {
-        Question:"2+2 ?",
-        options: ["3","4","5","6"],
-        correctAnswer:"4"
+        Question: "2+2 ?",
+        options: ["3", "4", "5", "6"],
+        correctAnswer: "4"
     },
     {
-        Question:"what is React",
-        options:["library","framework","API","Data Structure"],
-        correctAnswer:"library"
+        Question: "what is React",
+        options: ["library", "framework", "API", "Data Structure"],
+        correctAnswer: "library"
     }
 ]
-let currentQuestion= 0;
-let quesDiv= document.querySelector(".ques-box")
-let span= document.createElement("span")
-let ul= document.createElement("ul")
-let li= document.createElement("li")
+let currentQuestion = 0;
+let quesDiv = document.querySelector(".ques-box")
+let span = document.createElement("span")
+let ul = document.createElement("ul")
+let li = document.createElement("li")
 
 
-   let options= Data[0].options
-for(let i=0;i<options.length;i++){
-    let li= document.createElement("li")
-    li.innerText=options[i]
+let options = Data[currentQuestion].options
+for (let i = 0; i < options.length; i++) {
+    let li = document.createElement("li")
+    li.innerText = options[i]
     ul.append(li)
     console.log(li)
 
 
 }
-  const NextBtn=   document.createElement("button")
-  NextBtn.innerText="NEXT"
-  quesBox.append(NextBtn)
-span.innerText=Data[currentQuestion].Question
+const NextBtn = document.createElement("button")
+const BackBtn = document.createElement("button")
+BackBtn.innerText = "BACK"
+NextBtn.innerText = "NEXT"
+BackBtn.classList.add("back-btn")
+quesBox.append(NextBtn)
+span.innerText = Data[currentQuestion].Question
+
+const nextDiv = document.createElement("div")
+const contDiv = document.querySelector(".container")
+NextBtn.addEventListener("click", () => {
+    currentQuestion++
+
+    options
+    ul.innerHTML = ''
+    span.innerText = Data[currentQuestion].Question
+
+    for (let i = 0; i < options.length; i++) {
+        let li = document.createElement("li")
+        li.innerText = options[i]
+        ul.append(li)
+
+
+
+
+
+    }
+
+})
 
 
 
 const div = document.querySelector(".container")
 div.append(quesDiv)
-quesDiv.append(span,ul)
+quesDiv.append(span, ul, BackBtn, NextBtn)
