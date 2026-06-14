20baa6d49a208860ac01794c6e1f375b5393fe1a
Komal Priya <kryptonbenzene@gmail.com>
2026-06-14 21:47:58 +0530

    added number button and operator with input box

---
+++ JavaScript/Calculator/script.js
@@ -0,0 +1,15 @@
+
+
+const input= document.querySelector("input")
+
+const button= document.querySelectorAll("button")
+let num
+button.forEach((btn)=>{
+btn.addEventListener("click",()=>{
+
+    console.log(btn.innerText)
+   
+
+    
+})
+})
