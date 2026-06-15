242a25763cac29a2c8e9d73f48a473ba104b3751
Komal Priya <kryptonbenzene@gmail.com>
2026-06-15 16:23:53 +0530

    updated code numbers operator displayong in input box added backspace and dot operator

--- JavaScript/Calculator/script.js
+++ JavaScript/Calculator/script.js
@@ -2,13 +2,30 @@
 
 const input= document.querySelector("input")
 
-const button= document.querySelectorAll("button")
-let num
-button.forEach((btn)=>{
-btn.addEventListener("click",()=>{
 
-    console.log(btn.innerText)
+const buttons= document.querySelectorAll("button")
+let num
+buttons.forEach((btn)=>{
+btn.addEventListener("click",()=>{
+const display= document.querySelector(".display")
+const value = btn.innerText;
+
    
+if (value === "C") {
+   display.value = "";
+   return;
+}
+console.log(display.value)
+if (value === "=") {
+    try{
+ display.value = eval(display.value);
+    } catch{
+        input.value = 'Error';
+    }
+    return;
+}
+display.value +=value;
+
 
     
 })
