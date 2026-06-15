f84bc79638183ca8f8acaa835f9c89b2bed46d3e
Komal Priya <kryptonbenzene@gmail.com>
2026-06-15 17:39:26 +0530

    added the functionalities  of all the buttons , operators they  work now added  added keyboard eventlistener

--- JavaScript/Calculator/script.js
+++ JavaScript/Calculator/script.js
@@ -1,32 +1,75 @@
 
 
-const input= document.querySelector("input")
+const input = document.querySelector("input")
 
 
-const buttons= document.querySelectorAll("button")
-let num
-buttons.forEach((btn)=>{
-btn.addEventListener("click",()=>{
-const display= document.querySelector(".display")
-const value = btn.innerText;
+const buttons = document.querySelectorAll("button")
 
-   
-if (value === "C") {
-   display.value = "";
-   return;
-}
-console.log(display.value)
-if (value === "=") {
-    try{
- display.value = eval(display.value);
-    } catch{
-        input.value = 'Error';
+buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        // const display = document.querySelector(".display")
+        const value = btn.innerText;
+        const lastchar = input.value[input.value.length - 1]
+const operators= ["+","-","*"," /"]
+
+       
+     
+
+        switch(value) {
+
+            case "=":
+                try{
+                    input.value= eval(input.value);
+
+                }catch{
+                    input.value= '';
+                }
+                break;
+
+
+               case '⨯':
+
+               input.value= input.value.slice(0,-1)
+               break;
+
+               case 'C':
+                input.value= ''
+                break;
+
+            default:
+                if(operators.includes(value) &&
+            operators.includes(lastchar))
+            {
+                return;
+            }
+
+            input.value += value;
+        }
+
+
+
+
+
+
+    })
+})
+
+document.addEventListener("keydown", (event) => {
+  const key = event.key;
+
+  if ("0123456789+-*/.".includes(key)) {
+    input.value += key;
+  }
+
+  if (key === "Backspace") {
+    input.value = input.value.slice(0, -1);
+  }
+
+  if (key === "Enter") {
+    try {
+      input.value = eval(input.value);
+    } catch {
+      input.value = "";
     }
-    return;
-}
-display.value +=value;
-
-
-    
-})
-})
+  }
+});
