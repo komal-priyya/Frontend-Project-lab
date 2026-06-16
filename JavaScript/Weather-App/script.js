0ecf05f5decaa441b5694368dd43fcf22624cb9e
Komal Priya <kryptonbenzene@gmail.com>
2026-06-16 21:57:13 +0530

    updated code

---
+++ JavaScript/Weather-App/script.js
@@ -0,0 +1,24 @@
+    const button= document.querySelector("button")
+
+    const cityInput= document.querySelector('input')
+
+
+
+async function getData(url){
+    const response = await fetch(url);
+    const data= await response.text();
+    console.log(data)
+  return data  
+}
+
+getData();
+
+button.addEventListener("click" , async ()=>{
+const city= cityInput.value
+
+  const url =
+        `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`;
+   let results= getData(url)
+})
+
+
