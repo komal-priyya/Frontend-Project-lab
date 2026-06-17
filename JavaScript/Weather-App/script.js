8e010fc8239dadd052627d6483dc5c0a5a234ec5
Komal Priya <kryptonbenzene@gmail.com>
2026-06-17 15:22:29 +0530

    feched api  added the temperature himidity and city  added search functionalities

--- JavaScript/Weather-App/script.js
+++ JavaScript/Weather-App/script.js
@@ -1,24 +1,53 @@
-    const button= document.querySelector("button")
+const button = document.querySelector("button");
 
-    const cityInput= document.querySelector('input')
+const cityInput = document.querySelector('input');
 
 
 
-async function getData(url){
+async function getData(url) {
     const response = await fetch(url);
-    const data= await response.text();
-    console.log(data)
-  return data  
+    const data = await response.json();
+    console.log(data);
+
+
+    return data;
 }
 
-// getData();
+button.addEventListener("click", async () => {
+    const city = cityInput.value;
 
-button.addEventListener("click" , async ()=>{
-const city= cityInput.value
-
-  const url =
+    const url =
         `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`;
-   let results= getData(url)
+
+    let geoData = await getData(url);
+    if (!geoData) {
+        alert("city not found")
+    };
+    console.log(geoData);
+    const lat = geoData.results[0].latitude;
+    const lon = geoData.results[0].longitude;
+    console.log(lat, lon);
+
+    const weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,wind_speed_10m`;
+
+    const WeatherResponse = await fetch(weatherUrl);
+    const weatherData = await WeatherResponse.json();
+    console.log(weatherData);
+
+    const cityname = document.querySelector(".cityname");
+    const temp = document.querySelector(".temp");
+    const humidity = document.querySelector(".humidity");
+    const weatherIcon = document.querySelector(".weather-icon");
+    const windSpeed = document.querySelector(".wind-speed");
+    const dateTime = document.querySelector(".date-time");
+   
+
+    cityname.innerText= city;
+    temp.innerText= `Temperature: ${weatherData.current.temperature_2m}°C`;
+   humidity.innerText=`Humidity : ${weatherData.current.relative_humidity_2m}%`;
+
+    
+
 })
 
 
