7acad2e351a5a1657e137d71ea501ae2ceb8d296
Komal Priya <kryptonbenzene@gmail.com>
2026-06-24 19:05:39 +0530

    added the createNoteCard function , added the localstorage  also added the dot button and edit, delete button

--- JavaScript/Notes-App/script.js
+++ JavaScript/Notes-App/script.js
@@ -1,34 +1,85 @@
 const addBtn = document.querySelector(".add-btn")
 
-addBtn.addEventListener("click", () => {
-   
-    const notesDiv = document.createElement("div")
+
+const titlefield = document.querySelector(".title-field")
+const descriptionfield = document.querySelector(".description-field")
+
+function  createNoteCard(note){
+ const notesDiv = document.createElement("div")
     notesDiv.classList.add("noteCard");
 
     const title = document.createElement("span")
     // title.innerText="title"
     title.classList.add("title")
+
     const description = document.createElement("p")
     // description.innerText="description"
+
     description.classList.add("description")
-    const titlefield = document.querySelector(".title-field")
-    const descriptionfield = document.querySelector(".description-field")
-    title.innerText = titlefield.value;
-    description.innerText = descriptionfield.value
+    const dotbutton = document.createElement("button")
+    dotbutton.innerText = " ⋮"
+    title.innerText = `Title : ${note.title}`;
+    description.innerText = `Description : ${note.description}`
+    const btn = document.createElement("button")
 
-
-    notesDiv.append(title, description)
+    notesDiv.append(title, description, dotbutton)
     const notesbox = document.querySelector(".notes-box")
     notesbox.append(notesDiv)
- 
-     titlefield.value=""
-     descriptionfield.value=""
+}
 
-     if(titlefield && description == ""){
-        return 
-     }
+let savedNotes = JSON.parse(localStorage.getItem("notes"));
+// let savedNotes = JSON.parse(localStorage.getItem("notes"));
 
-    // const title= document.querySelector("span")
-    // const description=document.querySelector("p")
+if(savedNotes){
+    for(let i = 0; i < savedNotes.length; i++){
+        createNoteCard(savedNotes[i]);
+    }
+}
 
-})   
+addBtn.addEventListener("click", () => {
+
+    if (titlefield.value.trim() == '' || descriptionfield.value.trim() == '') {
+        return;
+    }
+let notes ;
+
+
+// let savedNotes =JSON.parse(localStorage.getItem("notes"))
+
+
+
+
+
+
+
+if(savedNotes){
+    notes=savedNotes;
+    console.log(notes)
+}else{
+    notes=[]
+}
+
+let note = {
+    title: titlefield.value,
+    description: descriptionfield.value,
+}
+
+notes.push(note)
+console.log(notes)
+localStorage.setItem("notes", JSON.stringify(notes))
+
+createNoteCard(note)
+   
+
+    titlefield.value = ""
+    descriptionfield.value = ""
+
+
+
+})
+
+
+
+
+   
+
