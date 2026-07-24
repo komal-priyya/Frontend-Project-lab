e58b5fec28afc6f6e393af9624bd36ed96936e84
Komal Priya <kryptonbenzene@gmail.com>
2026-07-24 20:10:24 +0530

    refactor: Improve expense card structure and styling

--- JavaScript/Expense-Tracker/script.js
+++ JavaScript/Expense-Tracker/script.js
@@ -1,38 +1,50 @@
+// const { createElement } = require("react");
 
 
-const Title= document.querySelector(".title");
-const Amount= document.querySelector(".amount");
+const Title = document.querySelector(".title");
+const Amount = document.querySelector(".amount");
 const Category = document.querySelector(".category");
 const Date = document.querySelector(".date");
 
 
-const addBtn= document.querySelector(".add-btn")
+const addBtn = document.querySelector(".add-btn")
 
 
-addBtn.addEventListener("click", ()=>{
+addBtn.addEventListener("click", () => {
 
-    const Expense= document.createElement("div")
-        Expense.classList.add("ExpsenseCard")
+    const Expense = document.createElement("div")
+    Expense.classList.add("ExpsenseCard")
 
-          
-        const titleElement= document.createElement("span")
-        const dateElement= document.createElement("span")
-        const categoryElement= document.createElement("span")
-        const AmountElement= document.createElement("span")
-const deleteBtn = document.createElement("button")
-const deleteBtnImage= document.createElement("img")
-deleteBtnImage.src= "assets/deletebutton.webp"
 
-deleteBtn.appendChild(deleteBtnImage)
+    const titleElement = document.createElement("span")
+    const dateElement = document.createElement("span")
+    const categoryElement = document.createElement("span")
+    const AmountElement = document.createElement("span")
 
-titleElement.innerText= Title.value
-dateElement.innerText= Date.value
-AmountElement.innerText= Amount.value
 
-Expense.append(titleElement,dateElement,categoryElement,AmountElement,deleteBtn)
-console.log("first")
-const AllExpensesBox= document.querySelector(".all-Expenses")
-AllExpensesBox.append(Expense)         
+
+    const deleteBtn = document.createElement("button")
+    const deleteBtnImage = document.createElement("img")
+    deleteBtnImage.src = "assets/deletebutton.webp"
+
+    deleteBtn.appendChild(deleteBtnImage)
+    const editbtn= document.createElement("button")
+    editbtn.innerText= "edit"
+
+    const dotSpan= document.createElement("span")
+    dotSpan.innerText=" :"
+    dotSpan.classList.add("dot")
+    const dotDiv= document.createElement("div")
+    dotDiv.append(editbtn,deleteBtn)
+    dotSpan.appendChild(dotDiv);
+    titleElement.innerText = Title.value
+    dateElement.innerText = Date.value
+    AmountElement.innerText = Amount.value
+
+    Expense.append(titleElement, dateElement, categoryElement, AmountElement, dotSpan)
+    console.log("first")
+    const AllExpensesBox = document.querySelector(".all-Expenses")
+    AllExpensesBox.append(Expense)
 
 
 })
